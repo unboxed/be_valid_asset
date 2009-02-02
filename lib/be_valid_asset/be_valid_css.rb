b68c789e87028443720e4bae8c02c4aab715ff15
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2009-02-02 12:13:49 +0000

    CSS validation is now functional with caching.

--- lib/be_valid_asset/be_valid_css.rb
+++ lib/be_valid_asset/be_valid_css.rb
@@ -2,67 +2,39 @@ require 'net/http'
 
 module BeValidAsset
 
-  Configuration.css_validator_host = 'validator.w3.org'
-  Configuration.css_validator_path = '/check'
+  Configuration.css_validator_host = 'jigsaw.w3.org'
+  Configuration.css_validator_path = '/css-validator/validator'
 
   class BeValidCss
   
     def initialize
     end
   
-    # Assert that markup (html/xhtml) is valid according the W3C validator web service.
-    # By default, it validates the contents of @response.body, which is set after calling
-    # one of the get/post/etc helper methods. You can also pass it a string to be validated.
-    # Validation errors, if any, will be included in the output. The input fragment and 
-    # response from the validator service will be cached in the $RAILS_ROOT/tmp directory to 
-    # minimize network calls.
-    #
-    # For example, if you have a FooController with an action Bar, put this in foo_controller_test.rb:
-    #
-    #   def test_bar_valid_markup
-    #     get :bar
-    #     assert_valid_markup
-    #   end
-    #
-  
     def matches?(fragment)
-      # fragment = response.body
-    
+
       return true if validity_checks_disabled?
 
-      if fragment.blank?
+      if fragment.empty?
         @message = "Response was blank (maybe a missing integrate_views)"
         return false
       end
 
-      params = [
-        file_to_multipart('file','file.css','text/css',fragment),
-        text_to_multipart('warning','1'),
-        text_to_multipart('profile','css2'),
-        text_to_multipart('usermedium','all') ]
-      
-      boundary = '-----------------------------24464570528145'
-      query = params.collect { |p| '--' + boundary + "\r\n" + p }.join('') + '--' + boundary + "--\r\n"
-      
-      response = Net::HTTP.start(Configuration.css_validator_host).post2(Configuration.css_validator_path, query, "Content-type" => "multipart/form-data; boundary=" + boundary)
+      response = get_validator_response(fragment)
 
       markup_is_valid = response['x-w3c-validator-status'] == 'Valid'
       @message = ''
       
-      puts response.body
       unless markup_is_valid
         fragment.split($/).each_with_index{|line, index| @message << "#{'%04i' % (index+1)} : #{line}#{$/}"} if Configuration.display_invalid_content
-        @message << XmlSimple.xml_in(response.body)['messages'][0]['msg'].collect{ |m| "Invalid markup: line #{m['line']}: #{CGI.unescapeHTML(m['content'])}" }.join("\n")
+        REXML::Document.new(response.body).root.each_element('//m:error') do |e|
+          @message << "Invalid css: line #{e.elements['m:line'].text}: #{e.elements['m:message'].get_text.value.strip}\n"
+        end
       end
-      if markup_is_valid
-        return true
-      else
-        return false
-      end
+      return markup_is_valid
     end
   
     def description
-      "be valid xhtml"
+      "be valid css"
     end
   
     def failure_message
@@ -78,13 +50,40 @@ module BeValidAsset
         ENV["NONET"] == 'true'
       end
 
-      def text_to_multipart(key,value)
-        return "Content-Disposition: form-data; name=\"#{CGI::escape(key)}\"\r\n\r\n#{value}\r\n"
+      def get_validator_response(fragment)
+        boundary = Digest::MD5.hexdigest(Time.now.to_s)
+        data = encode_multipart_params(boundary, :text => fragment, :output => 'soap12')
+        if Configuration.enable_caching
+          unless File.directory? Configuration.cache_path
+            FileUtils.mkdir_p Configuration.cache_path
+          end
+          digest = Digest::MD5.hexdigest(fragment)
+          cache_filename = File.join(Configuration.cache_path, "css-#{digest}")
+          if File.exist? cache_filename
+            response = File.open(cache_filename) {|f| Marshal.load(f) }
+          else
+            response = Net::HTTP.start(Configuration.css_validator_host).post2(Configuration.css_validator_path, data, "Content-type" => "multipart/form-data; boundary=#{boundary}")
+            if response.is_a? Net::HTTPSuccess
+              File.open(cache_filename, 'w') {|f| Marshal.dump(response, f) }
+            end
+          end
+        else
+          response = Net::HTTP.start(Configuration.css_validator_host).post2(Configuration.css_validator_path, data, "Content-type" => "multipart/form-data; boundary=#{boundary}")
+        end
+        return response
       end
 
-      def file_to_multipart(key,filename,mime_type,content)
-        return "Content-Disposition: form-data; name=\"#{CGI::escape(key)}\"; filename=\"#{filename}\"\r\n" +
-                  "Content-Transfer-Encoding: binary\r\nContent-Type: #{mime_type}\r\n\r\n#{content}\r\n"
+      def encode_multipart_params(boundary, params = {})
+        ret = ''
+        params.each do |k,v|
+          unless v.empty?
+            ret << "\r\n--#{boundary}\r\n"
+            ret << "Content-Disposition: form-data; name=\"#{k.to_s}\"\r\n\r\n"
+            ret << v
+          end
+        end
+        ret << "\r\n--#{boundary}--\r\n"
+        ret
       end
   
   end
