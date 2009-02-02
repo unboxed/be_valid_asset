85cee8a78b27aa63c2f03ec21500ad514e163fdc
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2009-02-02 15:48:19 +0000

    Added some more CSS tests.

--- spec/be_valid_asset/be_valid_css_spec.rb
+++ spec/be_valid_asset/be_valid_css_spec.rb
@@ -18,7 +18,30 @@ describe 'be_valid_css' do
         css.should be_valid_css
       }.should raise_error(SpecFailed, /expected css to be valid, but validation produced these errors/)
     end
-  
+
+    it "should fail unless resposne is HTTP OK" do
+      css = get_file('valid.css')
+
+      r = Net::HTTPServiceUnavailable.new('1.1', 503, 'Service Unavailable')
+      h = Net::HTTP.new(BeValidAsset::Configuration.css_validator_host)
+      h.stub!(:post2).and_return(r)
+      Net::HTTP.stub!(:start).and_return(h)
+
+      lambda {
+        css.should be_valid_css
+      }.should raise_error
+    end
+
+    it "should mark test as pending if ENV['NONET'] is true" do
+      ENV['NONET'] = 'true'
+
+      css = get_file('valid.css')
+      lambda {
+        css.should be_valid_css
+      }.should raise_error(Spec::Example::ExamplePendingError)
+
+      ENV.delete('NONET')
+    end
   end
   
   describe "with caching" do
@@ -30,14 +53,14 @@ describe 'be_valid_css' do
       BeValidAsset::Configuration.enable_caching = false
     end
 
-    it "should validate a valid string and cache the response" do
+    it "should validate valid css and cache the response" do
       css = get_file('valid.css')
       count = Dir.glob(BeValidAsset::Configuration.cache_path + '/*').size
       css.should be_valid_css
       Dir.glob(BeValidAsset::Configuration.cache_path + '/*').size.should eql(count + 1)
     end
 
-    it "should validate a valid string using the cached response" do
+    it "should validate valid css using the cached response" do
       css = get_file('valid.css')
       css.should be_valid_css
 
@@ -45,7 +68,7 @@ describe 'be_valid_css' do
       css.should be_valid_css
     end
 
-    it "should not validate an invalid response, but still cache the response" do
+    it "should not validate invalid css, but still cache the response" do
       css = get_file('invalid.css')
       count = Dir.glob(BeValidAsset::Configuration.cache_path + '/*').size
       lambda {
@@ -54,7 +77,7 @@ describe 'be_valid_css' do
       Dir.glob(BeValidAsset::Configuration.cache_path + '/*').size.should eql(count + 1)
     end
 
-    it "should not validate an invalid response, but use the cached response" do
+    it "should not validate invalid css, but use the cached response" do
       css = get_file('invalid.css')
       css.should_not be_valid_css
 
@@ -62,6 +85,21 @@ describe 'be_valid_css' do
       lambda {
         css.should be_valid_css
       }.should raise_error(SpecFailed, /expected css to be valid, but validation produced these errors/)
+    end
+
+    it "should not cache the result unless it is an HTTP OK response" do
+      css = get_file('valid.css')
+      count = Dir.glob(BeValidAsset::Configuration.cache_path + '/*').size
+
+      r = Net::HTTPServiceUnavailable.new('1.1', 503, 'Service Unavailable')
+      h = Net::HTTP.new(BeValidAsset::Configuration.css_validator_host)
+      h.stub!(:post2).and_return(r)
+      Net::HTTP.stub!(:start).and_return(h)
+
+      lambda {
+        css.should be_valid_css
+      }.should raise_error
+      Dir.glob(BeValidAsset::Configuration.cache_path + '/*').size.should eql(count)
     end
   end
 end
