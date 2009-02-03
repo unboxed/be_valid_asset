d3b4813fe928bab230887592e23ab7a7fd1076b7
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2009-02-03 09:20:26 +0000

    Refactored parameter encoding.

--- lib/be_valid_asset/be_valid_base.rb
+++ lib/be_valid_asset/be_valid_base.rb
@@ -27,26 +27,26 @@ module BeValidAsset
       end
 
       def get_validator_response(query_params = {})
-        boundary = Digest::MD5.hexdigest(Time.now.to_s)
-        data = encode_multipart_params(boundary, query_params)
         if Configuration.enable_caching
           digest = Digest::MD5.hexdigest(query_params.to_a.sort {|a,b| a[0].to_s<=>b[0].to_s}.join)
           cache_filename = File.join(Configuration.cache_path, digest)
           if File.exist? cache_filename
             response = File.open(cache_filename) {|f| Marshal.load(f) }
           else
-            response = call_validator( data, boundary )
+            response = call_validator( query_params )
             File.open(cache_filename, 'w') {|f| Marshal.dump(response, f) } if response.is_a? Net::HTTPSuccess
           end
         else
-          response = call_validator( data, boundary)
+          response = call_validator( query_params )
         end
         raise "HTTP error: #{response.code}" unless response.is_a? Net::HTTPSuccess
         return response
       end
 
-      def call_validator(data, boundary)
+      def call_validator(query_params)
         check_net_enabled
+        boundary = Digest::MD5.hexdigest(Time.now.to_s)
+        data = encode_multipart_params(boundary, query_params)
         return Net::HTTP.start(validator_host).post2(validator_path, data, "Content-type" => "multipart/form-data; boundary=#{boundary}" )
       end
 
